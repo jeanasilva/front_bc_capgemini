aa70669accf5e63f809d970b76bf95601ee84565
jeanasilva <jeansoftware67@gmail.com>
2020-09-22 21:54:26 -0400

    Inicializando projeto Front com Vuejs + Axios + VueCLI

---
+++ bc_capgemini/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+
+    <nav>
+      <div class="nav-wrapper blue darken-1">
+        <a href="#" class="brand-logo center">Operações Financeiras</a>
+      </div>
+    </nav>
+
+    <div class="container">
+      <div class="center-align">
+        <div class="col s9">
+
+        <br>
+        <a class="waves-effect waves-light btn-large"><i class="material-icons left">cloud</i>Consulta Saldo</a>
+        <br>
+        <br>
+        <a class="waves-effect waves-light btn-large"><i class="material-icons left">cloud</i>Depositar</a>
+        <br>
+        <br>
+        <a class="waves-effect waves-light btn-large"><i class="material-icons left">cloud</i>Sacar</a>
+
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+
+</script>
+
+
